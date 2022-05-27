4cb70ca8f34e33d7ca5245d4f1c51a561504f21f
lo1cgsan <lo1cgsan@gmail.com>
2022-05-27 12:44:28 +0200

    zmiany nazw pól i dodanie szablonów użytkownika

--- todor/auth.py
+++ todor/auth.py
@@ -5,32 +5,32 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from todor.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        email = request.form['email']
+        haslo = request.form['haslo']
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
+        if not email:
+            error = 'Email jest wymagany.'
+        elif not haslo:
+            error = 'Haśło jest wymagane.'
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO users (email, haslo) VALUES (?, ?)",
+                    (email, generate_password_hash(haslo)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} is already registered."
+                error = f"Adres {email} jest już zarejestrowany."
             else:
                 return redirect(url_for("auth.login"))
 
@@ -42,18 +42,18 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        email = request.form['email']
+        haslo = request.form['haslo']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM users WHERE email = ?', (email,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+            error = 'Błędny email.'
+        elif not check_password_hash(user['haslo'], haslo):
+            error = 'Błędne hasło.'
 
         if error is None:
             session.clear()
@@ -74,7 +74,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
+            'SELECT * FROM users WHERE id = ?', (user_id,)
         ).fetchone()
 
 
