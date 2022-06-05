e5723384df721f96ec338954847904029d8b1403
lo1cgsan <lo1cgsan@github.com>
2022-06-05 22:05:09 +0200

    zmiany todof

--- todof/todo.py
+++ todof/todo.py
@@ -2,14 +2,12 @@ from flask import Flask, g
 from flask import render_template
 import os
 import sqlite3
-import click
 from flask import current_app
-from flask.cli import with_appcontext
+import functools
 from datetime import datetime
 from flask import flash, redirect, url_for, request
 from flask import session
-from werkzeug.security import check_password_hash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
@@ -22,9 +20,11 @@ app.config.from_mapping(
 
 def get_db():
     """Funkcja tworząca połączenie z bazą danych"""
-    if not g.get('db'):  # jeżeli brak połączenia, to je tworzymy
-        # zapisujemy połączenie w kontekście aplikacji
-        g.db = sqlite3.connect(app.config['DATABASE'])
+    if 'db' not in g:  # jeżeli brak połączenia, to je tworzymy
+        g.db = sqlite3.connect(
+            app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )  # tworzymy i zapisujemy połączenie w kontekście aplikacji
         g.db.row_factory = sqlite3.Row
     return g.db  # zwracamy połączenie z bazą
 
@@ -44,15 +44,6 @@ def init_db():
     with current_app.open_resource('baza.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
-# @click.command('init-db')
-# @with_appcontext
-# def init_db_command():
-#     """Usunięcie danych i utworzenie nowych tabel."""
-#     init_db()
-#     click.echo('Inicjacja bazy danych.')
-
-# app.cli.add_command(init_db_command)
 
 @app.route('/')
 def index():
@@ -78,34 +69,11 @@ def zadania():
         error = 'Nie możesz dodać pustego zadania!'  # komunikat o błędzie
     if "user_id" in session:
         db = get_db()
-        kursor = db.execute('SELECT * FROM zadania ORDER BY data_pub DESC;')
-        zadania = kursor.fetchall()
+        zadania = db.execute('SELECT * FROM zadania ORDER BY data_pub DESC').fetchall()
         return render_template('zadania_lista.html', zadania=zadania, error=error)
     else:
         flash('Dodawanie zadań wymaga logowania.')
         return redirect(url_for('loguj'))
-
-
-@app.route('/zrobione', methods=['POST'])
-def zrobione():
-    """Zmiana statusu zadania na wykonane."""
-    zadanie_id = request.form['id']
-    db = get_db()
-    db.execute('UPDATE zadania SET zrobione=1 WHERE id=?', [zadanie_id])
-    db.commit()
-    flash('Zmieniono status zadania.')
-    return redirect(url_for('zadania'))
-
-
-@app.route('/niezrobione', methods=['POST'])
-def niezrobione():
-    """Zmiana statusu zadania na wykonane."""
-    zadanie_id = request.form['id']
-    db = get_db()
-    db.execute('UPDATE zadania SET zrobione=0 WHERE id=?', [zadanie_id])
-    db.commit()
-    flash('Zmieniono status zadania.')
-    return redirect(url_for('zadania'))
 
 
 @app.route('/rejestruj', methods=['GET', 'POST'])
@@ -175,6 +143,13 @@ def load_logged_in_user():
         ).fetchone()
 
 
+@app.route('/wyloguj')
+def wyloguj():
+    flash(f"Wylogowano użytkownika {session['email']}.")
+    session.clear()
+    return redirect(url_for('index'))
+
+
 # dekorator, który sprawdza, czy użytkownik jest zalogowany
 def login_required(view):
     @functools.wraps(view)
@@ -187,11 +162,72 @@ def login_required(view):
     return wrapped_view
 
 
-@app.route('/wyloguj')
-def wyloguj():
-    flash(f"Wylogowano użytkownika {session['email']}.")
-    session.clear()
+def get_zadanie(id, check_author=True):
+    zadanie = get_db().execute(
+        'SELECT z.id, zadanie, zrobione, data_pub, id_user, email'
+        ' FROM zadania z JOIN users u ON z.id_user = u.id'
+        ' WHERE z.id = ?',
+        (id,)
+    ).fetchone()
+
+    if zadanie is None:
+        abort(404, f"Zadanie id {id} nie istnieje.")
+
+    if check_author and zadanie['id_user'] != g.user['id']:
+        abort(403)
+
+    return zadanie
+
+
+@app.route('/<int:id>/edytuj', methods=('GET', 'POST'))
+@login_required
+def edytuj(id):
+    zadanie = get_zadanie(id)
+    print(zadanie['zadanie'])
+    if request.method == 'POST':
+        zadanie = request.form['zadanie']
+        error = None
+
+        if not zadanie:
+            error = 'Treść zadania nie może być pusta.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE zadania SET zadanie = ?'
+                ' WHERE id = ?',
+                (zadanie, id)
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('edytuj.html', zadanie=zadanie)
+
+
+@app.route('/<int:id>/usun', methods=('POST',))
+@login_required
+def usun(id):
+    get_zadanie(id)
+    db = get_db()
+    db.execute('DELETE FROM zadania WHERE id = ?', (id,))
+    db.commit()
     return redirect(url_for('index'))
+
+
+@app.route('/<int:id>/<int:status>/zmien_status', methods=('POST',))
+@login_required
+def zmien_status(id, status):
+    get_zadanie(id)
+    db = get_db()
+    db.execute(
+        'UPDATE zadania SET zrobione = ?'
+        ' WHERE id = ?',
+        (status, id)
+    )
+    db.commit()
+    return redirect(url_for('zadania'))
 
 
 with app.app_context():
