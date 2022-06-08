5211731ed790d1c64048772070133e5d072e98b2
lo1cgsan <lo1cgsan@github.com>
2022-06-08 22:01:21 +0200

    testy uzupełnienia

--- tests/data.sql
+++ tests/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO user (email, haslo)
 VALUES
   ('adres1@wp.pl', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('adres2@wp.pl', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+  ('adres1@wp.pl', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
 
 INSERT INTO zadania (id_user, zadanie, data_pub)
 VALUES (1, 'test' || x'0a' || 'body', '2018-01-01 00:00:00');
--- tests/test_zadania.py
+++ tests/test_zadania.py
@@ -10,7 +10,41 @@ def test_index(client, auth):
     auth.login()
     response = client.get('/')
     assert b'Wyloguj się' in response.data
-    assert b'test' in response.data
-    assert b'by test on 2018-01-01' in response.data
+    assert b'adres1@wp.pl' in response.data
+    assert b'2018-01-01' in response.data
     assert b'test\nbody' in response.data
-    assert b'href="/1/update"' in response.data
+    assert b'href="/1/edytuj"' in response.data
+
+
+@pytest.mark.parametrize('path', (
+    '/dodaj',
+    '/1/edytuj',
+    '/1/usun',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers["Location"] == "/auth/login"
+
+
+def test_author_required(app, client, auth):
+    # change the post author to another user
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE zadania SET id_user = 2 WHERE id = 1')
+        db.commit()
+
+    auth.login()
+    # current user can't modify other user's post
+    assert client.post('/1/edytuj').status_code == 403
+    assert client.post('/1/usun').status_code == 403
+    # current user doesn't see edit link
+    assert b'href="/1/edytuj"' not in client.get('/').data
+
+
+@pytest.mark.parametrize('path', (
+    '/2/edytuj',
+    '/2/usun',
+))
+def test_exists_required(client, auth, path):
+    auth.login()
+    assert client.post(path).status_code == 404
